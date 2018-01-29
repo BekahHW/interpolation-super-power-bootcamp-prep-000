ac4cc263763331909ff0a4389b2f962cf9fb84bd
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-29 18:31:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,5 +4,6 @@ def display_rainbow(color)
   puts colors.join(", ")
 end
 
-color = 
+color = "R: #{red[0]}, O: #{orange[1]}, Y: #{yellow[2]}, G: #{green[3]}, B: #{blue[4]}, I: #{indigo[5]}, V: #{violet[6]}"
 
+print_display_rainbow
