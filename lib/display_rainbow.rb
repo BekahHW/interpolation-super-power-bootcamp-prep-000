824008cc7a4e4fe4b71bec73eff20d2ac931d491
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-29 18:18:11 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,9 @@
 # Write your #display_rainbow method here
 
-def display_rainbow(colors)
+def display_rainbow
   puts "R: #{red[0]}, O: #{orange[1]}, Y: #{yellow[2]}, G: #{green[3]}, B: #{blue[4]}, I: #{indigo[5]}, V: #{violet[6]}"
 end
 
-roygbiv = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-print_display_rainbow(roygbiv)
+print_display_rainbow
